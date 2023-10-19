5b8ecf8e0f2cdb341b565734c202a2172e0f6c93
12hedia <hediasmida2@gmail.com>
2023-10-19 15:03:40 -0700

    fghj

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+nclude "lists.h"
+
+/**
+ *  * print_list - prints all the elements of a list_t
+ *   * @h: ptr to list
+ *    * Return: nbr of nodes
+ *     */
+
+size_t print_list(const list_t *h)
+{
+		const list_t *temp;
+			unsigned int count = 0;
+
+				temp = h;
+					while (temp)
+							{
+										if (!temp->str)
+													{
+																	printf("[0] (nil)\n");
+													}														}
+else
+															{
+															printf("[%u] %s\n", temp->len, temp->str);
+															}
+count++;
+temp = temp->next;
+																	}
+
+return (count);
+}
---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+ection .data
+    message db "Hello, Holberton", 10
+
+section .text
+global main
+main:
+    ; Prepare syscall parameters
+    mov rax, 1         ; System call number: write
+    mov rdi, 1         ; File descriptor: stdout
+    lea rsi, [message] ; Load address of message
+    mov rdx, 17        ; Message length
+
+    ; Make the syscall
+    syscall
+
+    ; Exit the program
+    mov rax, 60        ; System call number: exit
+    xor rdi, rdi       ; Exit status: 0
+    syscall
